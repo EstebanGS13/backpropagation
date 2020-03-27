9c80abdbda2a21369b54632a1cf565a57d0edf62
Esteban <estebangsan13@gmail.com>
2020-03-27 18:01:39 -0500

    Agregada opcion para probar digitos

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sigmoid(z):
@@ -12,11 +13,13 @@ def sigmoid_derivative(s):
 class NeuralNetwork:
 
     def __init__(self, X, Y):
-        self.inputs = X
-        self.Y = Y
-        self.weights1 = np.random.rand(self.inputs.shape[1], 4)
-        self.weights2 = np.random.rand(self.Y.shape[1], 4)
-        self.outputs = np.zeros(Y.shape)
+        self.inputs = X  # Each row is a training example, each column is a feature [X1,..., X7] (10x7)
+        self.Y = Y  # The real output data (10x4)
+        self.hidden_nodes = 4  # Number of nodes in the hidden layer
+        self.weights1 = np.random.rand(self.inputs.shape[1], self.hidden_nodes)  # (7x4)
+        self.weights2 = np.random.rand(self.Y.shape[1], self.hidden_nodes)  # (4x4)
+        self.outputs = np.zeros(Y.shape)  # The net's output data (10x4)
+
         self.error_history = []
         self.epoch_list = []
 
@@ -25,30 +28,32 @@ class NeuralNetwork:
         self.outputs = sigmoid(np.dot(self.hidden, self.weights2))
 
     def backpropagation(self):
-        errors = self.Y - self.outputs
+        self.errors = self.Y - self.outputs
         derivatives2 = sigmoid_derivative(self.outputs)
-        deltas2 = errors * derivatives2
+        deltas2 = self.errors * derivatives2
         self.weights2 += np.dot(self.hidden.T, deltas2)
 
         derivatives1 = sigmoid_derivative(self.hidden)
         deltas1 = np.dot(deltas2, self.weights2.T) * derivatives1
         self.weights1 += np.dot(self.inputs.T, deltas1)
 
-    def train(self, epochs=25000):
-            for epoch in range(epochs):
-                self.feed_forward()
-                self.backpropagation()
-                #
-                # self.error_history.append(np.average(np.abs((self.error))))
-                # self.epoch_list.append(epoch)
+    def train(self, epochs=20000):
+        print(f"Training network ({epochs} epochs)...\n\n")
+        for epoch in range(epochs):
+            self.feed_forward()
+            self.backpropagation()
+
+            self.error_history.append(np.average(np.abs((self.errors))))
+            self.epoch_list.append(epoch)
 
     def predict(self, new_input):
+        # An additional feed_forward-like to make a prediction
         hidden = sigmoid(np.dot(new_input, self.weights1))
         prediction = sigmoid(np.dot(hidden, self.weights2))
-        return np.round(prediction, 4)
+        return np.round(prediction, 3)
 
 
-if __name__ == '__main__':
+def main():
     inputs = np.array([[1, 1, 1, 1, 1, 1, 0],
                        [0, 1, 1, 0, 0, 0, 0],
                        [1, 1, 0, 1, 1, 0, 1],
@@ -74,8 +79,30 @@ if __name__ == '__main__':
     NN = NeuralNetwork(inputs, outputs)
     NN.train()
 
-    example = np.array([[1, 1, 1, 1, 0, 0, 1]]) #3
-    example_2 = np.array([[1, 1, 1, 0, 0, 0, 1]]) #7
 
-    print(NN.predict(example), ' - Correct: 3 ', [0, 0, 1, 1])
-    print(NN.predict(example_2), ' - Correct: 7 ', [0, 1, 1, 1])
+
+    while True:
+        try:
+            test = int(input("Ingrese el digito el cual quiere que la red prediga"
+                             "(este sera entregado a la red en formato de 7 segmentos): "))
+            if 0 <= test <= 9:
+                segments = inputs[test]
+                prediction = NN.predict(segments)
+                expected = outputs[test]
+
+                print(f"Valor ingresado: {segments}\nValor predicho: {prediction}\nValor correcto: {expected}\n")
+            else:
+                print("Debe ingresar un solo digito")
+        except ValueError:
+            print("Debe ingresar un digito")
+            continue
+
+    # plot the error over the entire training duration
+    plt.figure(figsize=(10, 5))
+    plt.plot(NN.epoch_list, NN.error_history)
+    plt.xlabel('Epoch')
+    plt.ylabel('Error')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
